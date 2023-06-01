9be8c8cdb7b8c13449e71d10b6f2718775c6ce84
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-06-01 13:24:15 +0300

    Update App.jsx
    apply statistics with title if title exist and only statistics? if title is not exist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
-import user from '../data/user.json';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 
+import user from '../data/user.json';
+import data from '../data/data.json';
 export const App = () => {
   return (
     <>
@@ -11,6 +13,12 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      {data && data.length > 0 ? (
+        <>
+          {data.title && <Statistics title={data.title} stats={data} />}
+          {!data.title && <Statistics stats={data} />}
+        </>
+      ) : null}
     </>
   );
 };
