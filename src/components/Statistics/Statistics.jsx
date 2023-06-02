76d7131409cf0fd9bc12b483b58c4677c9a3ce77
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-06-02 12:24:55 +0300

    Update Statistics.jsx
    remove hardcode of h2 title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,7 +6,6 @@ function Statistics({ title = false, stats }) {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-      <h2 className={css.title}>Upload stats</h2>
       <ul className={css['stat-list']}>
         {stats.map(({ id, label, percentage }) => (
           <li
