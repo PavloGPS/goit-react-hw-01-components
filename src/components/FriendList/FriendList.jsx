4b2df3855da6f4d7ab54eb67edc1fe0f0fdcada3
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-06-03 00:41:45 +0300

    Create FriendList.jsx
    makes component FriendList with styles

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import FriendListItem from 'components/FriendListItem/FriendListItem';
+
+function FriendList({ friends }) {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          key={id}
+        />
+      ))}
+    </ul>
+  );
+}
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
+
+export default FriendList;
