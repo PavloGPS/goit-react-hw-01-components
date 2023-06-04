2d73a78b61f1d9a409b25f16736db7d53fa6a3b6
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-06-04 22:57:20 +0300

    applying camelCase
    changes css classes names on camelCase notation

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,7 +6,7 @@ function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-      <ul className={css['stat-list']}>
+      <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => (
           <li
             className={css.item}
