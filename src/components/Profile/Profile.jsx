3012843cce54549ebb8374f231cb59f9e16222e6
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-06-04 21:23:45 +0300

    Update Profile.jsx
    adds propTypes of object "stats"

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -34,7 +34,13 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.object,
+  stats: PropTypes.objectOf(
+    PropTypes.exact({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Profile;
